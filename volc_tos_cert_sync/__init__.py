70e3c237cde414e9241fa08cb966230e4f1b290d
hanfei.liu <hanfei.liu@xiao-ju.com>
2026-03-06 17:49:21 +0800

    add CERT_THRESHOLD_DAYS(optional) env support

--- volc_tos_cert_sync/__init__.py
+++ volc_tos_cert_sync/__init__.py
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Fundy"
 __email__ = "rabbitlhf@gmail.com"
